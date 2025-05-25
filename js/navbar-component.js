3c3976879197cf65aa737752e58bcea842bd9a0c
mminderbinder <92769832+mminderbinder@users.noreply.github.com>
2025-05-24 23:35:32 -0400

    Added services page. Started separating CSS

--- js/navbar-component.js
+++ js/navbar-component.js
@@ -2,16 +2,13 @@ function createNavbar() {
     return `
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">J & A Accounting</a>
+        <a class="navbar-brand" href="index.js.html">J & A Accounting</a>
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="index.js.html">Home</a>
+                <a class="nav-link" href="services.html">Services</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="about.html">About</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="services.html">Services</a>
             </li>
         </ul>
     </div>
